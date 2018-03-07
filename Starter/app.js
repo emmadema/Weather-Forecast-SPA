177c621c690b737c189c5f7ba8a62308c1bd8f2f
emmadema <emma.demango@gmail.com>
2018-03-06 22:37:58 -0700

    adding routes

--- Starter/app.js
+++ Starter/app.js
@@ -1,6 +1,24 @@
 //Module
 
-let weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
+let weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource', 'ngRoute']);
+
+//Routes
+
+weatherApp.config(function ($routeProvider){
+	$routeProvider
+
+//Route for main
+	.when('/', {
+		templateUrl: 'pages/home.html',
+		controller: 'mainController'
+	})
+
+//route for forecast
+	.when('/forecast', {
+		templateUrl: 'pages/forecast.html',
+		controller: 'forecastController'
+	});
+});
 
 //home page controller
 
@@ -10,6 +28,6 @@ weatherApp.controller('mainController', ['$scope', function($scope){
 
 //forcast page controller
 
-weatherApp.controller('forcastController', ['$scope', function($scope){
+weatherApp.controller('forecastController', ['$scope', function($scope){
 
 }]);
