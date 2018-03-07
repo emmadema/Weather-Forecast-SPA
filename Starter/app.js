ad0a526f1db2da2391740ca5535c06d99a7f3f05
emmadema <emma.demango@gmail.com>
2018-03-06 22:10:02 -0700

    first commit

---
+++ Starter/app.js
@@ -0,0 +1,15 @@
+//Module
+
+let weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
+
+//home page controller
+
+weatherApp.controller('mainController', ['$scope', function($scope){
+
+}]);
+
+//forcast page controller
+
+weatherApp.controller('forcastController', ['$scope', function($scope){
+
+}]);
