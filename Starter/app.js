ec1d3f7c2599a91dab170467d7f1ab81d0f772b6
emmadema <emma.demango@gmail.com>
2018-03-06 23:43:55 -0700

    adding directive

--- Starter/app.js
+++ Starter/app.js
@@ -62,6 +62,22 @@ weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParam
 	};
 }]);
 
+//Directives
+
+weatherApp.directive("weatherReport", function() {
+	return {
+		restrict: 'E', 
+		templateUrl: 'directives/weatherReport.html',
+		replace: true,
+		scope: {
+			weatherDay: "=",
+			convertToStandard: "&",
+			convertToDate: "&",
+			dateFormat: "@"
+		}
+	};
+});
+
 
 //
 
